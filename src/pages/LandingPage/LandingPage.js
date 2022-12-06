ac92c60639b319687b746feeff734e8ff8b18e60
User <Milosh@kuce-2.local>
2022-12-06 17:35:56 +0100

    Updating small fixes to development

--- src/pages/LandingPage/LandingPage.js
+++ src/pages/LandingPage/LandingPage.js
@@ -2,19 +2,19 @@ import React, { useContext } from "react";
 import DataContext from "../../utils/context";
 import CandidateCard from "../../components/Candidate/CandidateCard/CandidateCard";
 import "./landingPage.scss";
-import Search from "../../components/Search/Search"
+import Search from "../../components/Search/Search";
 
 const LandingPage = () => {
-  const {data, setData, searchValue} = useContext(DataContext);
+  const { data, setData, searchValue } = useContext(DataContext);
+  const search = data.filter((e) =>
+    e.name.toLowerCase().includes(searchValue.toLowerCase())
+  );
 
-  const search = data.filter(e => e.name.toLowerCase().includes(searchValue.toLowerCase()))
-  console.log(search);
-  
   return (
     <>
-     <div id="searchWrap">
-      <h2>Candidates</h2>
-      <Search />
+      <div id="searchWrap">
+        <h2>Candidates</h2>
+        <Search />
       </div>
       <div className="candidate-card-container">
         {search.map((e, i) => {
