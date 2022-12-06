4ec20ccdc322b7b6b7abf2d3a2a38af4aca57fb6
Petar Srdanovic <psrdanovic@yahoo.com>
2022-12-06 10:02:47 +0100

    Petar  test

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
 
 const App = () => {
-  const [data, setData] = useState([])
+  const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch('http://localhost:3333/api/candidates')
-    .then(res => res.json())
-    .then(res => console.log(res))
-  }, [])
-  
-  return (
-    <div>App</div>
-  )
-}
+    fetch("http://localhost:3333/api/candidates")
+      .then((res) => res.json())
+      .then((res) => console.log(res));
+  }, []);
 
-export default App
+  return <div>App</div>;
+};
+
+export default App;
