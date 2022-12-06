98ffadfeb384141322fcf77e59b44e290d075182
User <Milosh@kuce-2.local>
2022-12-06 10:34:49 +0100

    New development branch

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   useEffect(() => {
     fetch('http://localhost:3333/api/candidates')
     .then(res => res.json())
-    .then(res => setData(res))
+    .then(res => console.log(res))
   }, [])
   
   return (
