cba3e30ba7f2b2aa492289c2dbd4014bac2f44cc
Petar Srdanovic <psrdanovic@yahoo.com>
2022-12-06 14:56:51 +0100

    Fix candidates button route

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,14 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import './header.scss'
+import "./header.scss";
 
 const Header = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
-    <header id='header'>
+    <header id="header">
       <h1>Interviews Reports</h1>
       <ul>
-        <li onClick={() => navigate('/candidate')}>Candidates</li>
+        <li onClick={() => navigate("/")}>Candidates</li>
         <li>Log in</li>
       </ul>
     </header>
