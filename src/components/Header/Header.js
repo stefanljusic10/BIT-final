1112ef9ec0348b6b4384b5c0d8664b122348d495
unknown <stefanljusic10@gmail.com>
2022-12-06 11:43:54 +0100

    header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,7 @@
-import React, { useContext } from "react";
-import DataContext from "../../utils.js/context";
+import React from "react";
 import './header.scss'
 
 const Header = () => {
-  const data = useContext(DataContext)
-  console.log(data);
   return (
     <header id='header'>
       <h1>Interviews Reports</h1>
