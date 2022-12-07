1496cb84edc0bbdf4c2d1f73e91aadc0769ff86b
Petar Srdanovic <psrdanovic@yahoo.com>
2022-12-07 14:50:24 +0100

    Complete candidate personal info

--- src/pages/CandidatePage/CandidatePage.js
+++ src/pages/CandidatePage/CandidatePage.js
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import moment from "moment";
 import { useParams } from "react-router-dom";
+import DataContext from "../../utils/context";
+import "./candidatePage.scss";
 
-const CandidatePage = (props) => {
+const CandidatePage = () => {
+  const { data } = useContext(DataContext);
+
+  // useEffect(() => {
+  //   ({ data } = useContext(DataContext));
+  // }, []);
   const { id } = useParams();
-  console.log(id);
+
+  const chosenCandidate = data?.candidates?.find((e) => e.id == id);
+  console.log(chosenCandidate);
+
+  // console.log(id, data, chosenCandidate);
+  const email = chosenCandidate?.email.toLowerCase();
+
   return (
     <>
       <div className="personal-info">
@@ -11,21 +25,25 @@ const CandidatePage = (props) => {
         <div className="personal-info-data">
           <div>
             <p>Name:</p>
-            <p>{props.candidate.name}</p>
+            <span>{chosenCandidate?.name}</span>
           </div>
           <div>
             <p>Date of birth:</p>
-            <p>{props.candidate.birthday}</p>
+            <span>{moment(chosenCandidate?.birthday).format("LL")}</span>
           </div>
           <div>
             <p>Email:</p>
-            <p>{props.candidate.email}</p>
+            <span>{email}</span>
           </div>
           <div>
             <p>Education:</p>
-            <p>{props.candidate.education}</p>
+            <span>{chosenCandidate?.education}</span>
           </div>
         </div>
+      </div>
+
+      <div className="reports">
+        <h2>Reports</h2>
       </div>
     </>
   );
