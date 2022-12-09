f0e7dbc50cc7e18f32b4c5efc04565653d5b2979
Petar Srdanovic <psrdanovic@yahoo.com>
2022-12-09 11:00:19 +0100

    Fix displaying report cards

--- src/pages/CandidatePage/CandidatePage.js
+++ src/pages/CandidatePage/CandidatePage.js
@@ -9,14 +9,20 @@ const CandidatePage = () => {
   const { data } = useContext(DataContext);
   const { id } = useParams();
 
-  const chosenCandidate = data.candidates.find(e => e.id == id)
-  const chosenCandidateReports = data?.reports.filter((e) => e.candidateId === id);
+  const chosenCandidate = data.candidates.find((e) => e.id == id);
+  const chosenCandidateReports = data?.reports?.filter(
+    (e) => e.candidateId == id
+  );
   const email = chosenCandidate?.email.toLowerCase();
+  console.log(chosenCandidateReports);
 
   return (
     <>
       <div className="personal-info">
-        <img src="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492__340.jpg" alt={chosenCandidate?.name} />
+        <img
+          src="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492__340.jpg"
+          alt={chosenCandidate?.name}
+        />
         <div className="personal-info-data">
           <div>
             <p>Name:</p>
