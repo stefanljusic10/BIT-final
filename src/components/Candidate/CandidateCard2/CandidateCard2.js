e9f2e292ec5ee052cd033ae2fa39f20dcc28bb98
brocetta <broceta@yahoo.com>
2022-12-09 16:05:47 +0100

    fixing small bugs

--- src/components/Candidate/CandidateCard2/CandidateCard2.js
+++ src/components/Candidate/CandidateCard2/CandidateCard2.js
@@ -3,12 +3,13 @@ import { useNavigate } from "react-router-dom";
 import "./candidateCard2.scss"
 
 const CandidateCard = (props) => {
-  const navigate = useNavigate();
   return (
     <>
       <div
         className="candidate-card2"
-        onClick={()=>props.setCandidateSelected(props.e)}
+        onClick={()=>{
+          props.setCandidateSelected(props.e)
+        }}
       >
         <img src="https://cdn.pixabay.com/photo/2017/02/20/18/03/cat-2083492__340.jpg"></img>
         <div className="nameEmail">
