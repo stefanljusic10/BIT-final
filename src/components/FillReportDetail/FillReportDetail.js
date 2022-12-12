853c36f93ae3294445480f563629549810d82dd7
brocetta <broceta@yahoo.com>
2022-12-12 18:20:54 +0100

    Updating small details

--- src/components/FillReportDetail/FillReportDetail.js
+++ src/components/FillReportDetail/FillReportDetail.js
@@ -4,7 +4,26 @@ import './fillReportDetail.scss'
 const FillReportDetail = ({ candidateSelected }) => {
   return (
     <form className='fillReportDetail'>
-        {candidateSelected.name}
+       
+          <label className="interviewDate">
+            Interview Date
+        <input type="date" />
+          </label>
+        
+        <div className='phase'>
+        <div>Phase</div>
+        <select>
+          <option>Technical</option>
+          <option>HR</option>
+           </select>
+        </div>
+        <div className='status'>
+        <div>Status</div>
+        <select>
+          <option>Technical</option>
+          <option>HR</option>
+           </select>
+        </div>
     </form>
   )
 }
