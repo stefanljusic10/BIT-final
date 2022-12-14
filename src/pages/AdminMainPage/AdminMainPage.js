2bb5f3c8d13a6531deaef56eaec9373f592768f6
unknown <stefanljusic10@gmail.com>
2022-12-14 19:08:57 +0100

    search reports

--- src/pages/AdminMainPage/AdminMainPage.js
+++ src/pages/AdminMainPage/AdminMainPage.js
@@ -1,14 +1,16 @@
 import React, { useContext, useEffect, useState } from "react";
 import CandidateReport from "../../components/Candidate/CandidateReport/CandidateReport";
 import CandidateModal from "../../components/Candidate/CandidateModal/CandidateModal";
+import Search  from "../../components/Search/Search";
 import DataContext from "../../utils/context";
 import "./adminMainPage.scss";
 
 const AdminMainPage = () => {
-  const { data, reportId, setReportId, setCurrentPage } =
-    useContext(DataContext);
+  const { data, reportId, setReportId, setCurrentPage, searchValue } = useContext(DataContext);
   const listOfReports = data?.reports;
   const [isReportClicked, setIsReportClicked] = useState(false);
+  const searchReport = listOfReports.filter((e) => e.candidateName.toLowerCase().includes(searchValue.toLowerCase()) ||
+  e.companyName.toLowerCase().includes(searchValue.toLowerCase()));
 
   useEffect(() => {
     setCurrentPage("Reports");
@@ -17,7 +19,8 @@ const AdminMainPage = () => {
   return (
     <>
       <div className="admin-wrapper">
-        {listOfReports.map((e) => (
+        <Search />
+        {searchReport.map((e) => (
           <CandidateReport
             key={e.id}
             report={e}
